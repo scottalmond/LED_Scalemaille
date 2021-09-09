89ad2c2b1dea9987066d66482663770734a3b47d
scottalmond <scottalmond13@gmail.com>
2021-09-09 06:35:04 -0700

    v2 of shell

--- SW/test/strandtest/strandtest.ino
+++ SW/test/strandtest/strandtest.ino
@@ -20,7 +20,7 @@
 #define LED_PIN    A0
 
 // How many NeoPixels are attached to the Arduino?
-#define LED_COUNT 11*45
+#define LED_COUNT 13*45
 
 // Declare our NeoPixel strip object:
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
