a7abc1aeaf99a55d2e0068ca87e60b8e7fe0471d
scottalmond <scottalmond13@gmail.com>
2021-10-04 10:50:20 -0700

    v1r2 drafting

--- SW/test/rainbow_qtpy/rainbow_qtpy.ino
+++ SW/test/rainbow_qtpy/rainbow_qtpy.ino
@@ -17,7 +17,7 @@
 
 // Which pin on the Arduino is connected to the NeoPixels?
 // On a Trinket or Gemma we suggest changing this to 1:
-#define LED_PIN    A0
+#define LED_PIN    A2
 
 // How many NeoPixels are attached to the Arduino?
 #define LED_COUNT 13*10
